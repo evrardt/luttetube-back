e19675a3f557f72379453001614aab955fb1dc2e
evrardt <evrard.dev@gmail.com>
2016-08-17 16:24:07 +0200

    Revert "remove sortPlaylistByDate as it was not working and put "
    This reverts commit 508e7debb69ce119a0b3635c125f8b6af23c28d0.

--- server/app.js
+++ server/app.js
@@ -47,7 +47,21 @@ server.listen(config.port, config.ip, function () {
 
 	getPlaylistsArray(LUTTE_CHANNEL, YOUTUBE_API_KEY);
 
-	function seedDbForPlaylists(channel) {
+	function sortPlaylistsByDate(channel) {
+		playlists.sort(function(a,b) {
+			if (!b || !b.date) {
+				return 1;
+			}
+			if (!a || !a.date) {
+				return -1;
+			}
+			if (a.date.getTime() < b.date.getTime())
+				return 1;
+			if (a.date.getTime() > b.date.getTime())
+				return -1;
+			return 0;
+		});
+
 		if (channel === LUTTE_CHANNEL) {
 			var Place = require('./api/luttePlace/luttePlace.model');
 			var Type = require('./api/lutteType/lutteType.model');
@@ -92,7 +106,7 @@ server.listen(config.port, config.ip, function () {
 				}
 			});
 		} else {
-			seedDbForPlaylists(channel);
+			sortPlaylistsByDate(channel);
 		}
 	}
 
