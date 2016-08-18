1bc1869c0e83cd39f067d01f06656f0493db4687
evrardt <evrard.dev@gmail.com>
2016-08-18 02:52:05 +0200

    update

--- server/config/seed.js
+++ server/config/seed.js
@@ -6,7 +6,10 @@
 'use strict';
 // Insert seed models below
 var User = require('../api/user/user.model');
+var Submission = require('../api/submission/submission.model');
 
 User.find({}).remove(function() {
     User.create(require('../api/user/user.seed.json'));
 });
+
+Submission.find({}).remove();
