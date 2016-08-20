eb9d9a05fbf4a5b95b7c20a782d516b43ce066bf
evrardt <evrard.dev@gmail.com>
2016-08-20 19:50:49 +0200

    update

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ if(config.seedDB) { require('./config/seed'); }
 
 // Setup server
 var app = express();
-app.use(cors({origin:'http://luttetube.fr'}));
+app.use(cors());
 var server = require('http').createServer(app);
 require('./config/express')(app);
 require('./routes')(app);
